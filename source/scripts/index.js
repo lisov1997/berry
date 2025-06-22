cd7afa5f5766c8fc5e7866767c263d7834c04607
Sergey <serlisov1997@gmail.com>
2025-06-22 11:29:02 +0300

    3.1. Готовый проект
    -добавил полную стилизацию страниц index и catalog для 3 вариантов брейкпоинтов

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,14 @@
-/* в этот файл добавляет скрипты*/
+const nav = document.querySelector('.page-header__nav');
+const navToggler = document.querySelector('.page-header__nav-toggler');
+
+nav.classList.add('page-header__nav--closed');
+
+navToggler.addEventListener('click', () => {
+  if (nav.classList.contains('page-header__nav--closed')) {
+    nav.classList.remove('page-header__nav--closed');
+    nav.classList.add('page-header__nav--opened');
+  } else {
+    nav.classList.add('page-header__nav--closed');
+    nav.classList.remove('page-header__nav--opened');
+  }
+});
